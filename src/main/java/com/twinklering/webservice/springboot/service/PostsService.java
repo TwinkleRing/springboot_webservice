752795566a941b7d60c0c10efc7c640de5357d6d
TwinkleRing <dlrmwl15@gmail.com>
2020-09-02 22:22:09 +0900

    3장 jpa!

---
+++ src/main/java/com/twinklering/webservice/springboot/service/PostsService.java
@@ -0,0 +1,40 @@
+package com.twinklering.webservice.springboot.service;
+
+import com.twinklering.webservice.springboot.domain.posts.Posts;
+import com.twinklering.webservice.springboot.domain.posts.PostsRepository;
+import com.twinklering.webservice.springboot.web.dto.PostsResponseDto;
+import com.twinklering.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.twinklering.webservice.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+
+}
