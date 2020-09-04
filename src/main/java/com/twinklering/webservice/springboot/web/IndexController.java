8a98da1712dae9b24b9a1514f396ec80800fa3c3
TwinkleRing <dlrmwl15@gmail.com>
2020-09-04 17:55:54 +0900

    Oauth 적용

--- src/main/java/com/twinklering/webservice/springboot/web/IndexController.java
+++ src/main/java/com/twinklering/webservice/springboot/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.twinklering.webservice.springboot.web;
+import com.twinklering.webservice.springboot.config.auth.dto.SessionUser;
 import com.twinklering.webservice.springboot.domain.posts.Posts;
 import com.twinklering.webservice.springboot.service.PostsService;
 import com.twinklering.webservice.springboot.web.dto.PostsResponseDto;
@@ -8,14 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
@@ -31,5 +40,6 @@ public class IndexController {
 
         return "posts-update";
     }
+
 
 }
